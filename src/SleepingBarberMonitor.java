18192afd9dc91265b840cdad73ad9bf4bcf99f83
benSepanski <ben_sepanski@alumni.baylor.edu>
2020-09-04 10:41:12 -0500

    Added sleeping barber monitor

---
+++ src/SleepingBarberMonitor.java
@@ -0,0 +1,96 @@
+package monitorwarehouse;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Implements sleeping barber problem
+ * 
+ * A barber thread is calling cut_hair() over and over
+ * again, customer threads are calling get_haircut()
+ * 
+ * There must be N > 0 chairs in the waiting room
+ * 
+ */
+public class SleepingBarberMonitor {
+    private int max_free_seats, num_free_seats;
+    private boolean barber_cutting_hair;
+
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition customer_ready = lock.newCondition(),
+        barber_cuts_hair = lock.newCondition();
+
+    public SleepingBarberMonitor(int nchairs) {
+        max_free_seats = Math.max(1, nchairs);
+        num_free_seats = max_free_seats;  // start with empty shop
+        barber_cutting_hair = false;
+    }
+
+    /**
+     * If there are no customers, the barber sleeps in the
+     * cutting chair until they are awoken. They then
+     * take a customer from the waiting room and put them
+     * in the cutting chair and become busy (cutting their hair).
+     */
+    public void cut_hair() {
+        lock.lock();
+        try {
+            // While waiting room is empty, wait for a customer to
+            // wake me up (signal me)
+            while(num_free_seats >= max_free_seats) {
+                try{
+                    customer_ready.await();
+                }
+                catch(InterruptedException e) {
+                    throw new RuntimeException();
+                }
+            }
+            // Now I have a customer, take them from waiting room and
+            // begin to give them a haircut
+            num_free_seats++;
+            barber_cuts_hair.signal();
+            barber_cutting_hair = true;
+        }
+        finally {
+            lock.unlock();
+        }
+
+    }
+
+    /**
+     * If a customer enters a waiting room with room to sit
+     * and the barber is sleeping, they wake the barber up. 
+     * 
+     * If there is room in the waiting room, the customer
+     * waits in a chair until the barber is cutting their hair. 
+     * They then receive a haircut and release the barber to no
+     * longer be busy
+     */
+    public boolean get_haircut() {
+        lock.lock();
+        boolean got_haircut = false;
+        try {
+            // Now if there is room in the waiting room, wait
+            if(num_free_seats > 0) {
+                num_free_seats--;
+                // Let barber know to wake up!
+                customer_ready.signal();
+                try {
+                    while(!barber_cutting_hair) {
+                        barber_cuts_hair.await();
+                    }
+                }
+                catch(InterruptedException e) {
+                    throw new RuntimeException();
+                }
+                // We got our haircut, let's go
+                barber_cutting_hair = false;
+                got_haircut = true;
+            }
+        }
+        finally {
+            lock.unlock();
+        }
+        return got_haircut;
+    }
+}
